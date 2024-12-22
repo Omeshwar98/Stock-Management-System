3a44b73610de4be1fd4da1188a341aa08c1ceb27
Omeshwar Sharma <121169047+Omeshwar98@users.noreply.github.com>
2024-12-22 12:14:11 +0530

    Add files via upload

---
+++ Stock Management System/src/dao/StockUtils.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author oshar
+ */
+public class StockUtils {
+    public static String billPath = "D:\\";
+    
+}
